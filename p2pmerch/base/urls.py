b04a3de89b59cb88a1a2e1a3f888ffff2d9bbc42
MathwLll <polarbearice7@gmail.com>
2021-11-20 13:57:17 -0700

    updated navbar

--- p2pmerch/base/urls.py
+++ p2pmerch/base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", views.base, name="base"),
     path("home/", views.base, name="base"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
