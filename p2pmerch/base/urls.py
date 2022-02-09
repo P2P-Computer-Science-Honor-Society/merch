3a8485fcf6d06888f41e7c6610f21f63116a5c99
MathwLll <polarbearice7@gmail.com>
2022-02-08 21:45:27 -0700

    starting to make login page

--- p2pmerch/base/urls.py
+++ p2pmerch/base/urls.py
@@ -5,7 +5,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path("", views.home, name="home"),
     path("home/", views.home, name="home"),
-    path("base/", views.base, name="base")
+    path("base/", views.base, name="base"),
+    path("login/", views.login, name="login"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- p2pmerch/base/views.py
+++ p2pmerch/base/views.py
@@ -6,5 +6,8 @@ def base(response):
 def home(response):
     return render(response, "base/home.html")
 
+def login(response):
+    return render(response, "base/login.html")
+
 def test(response):
     return render(response, "base/test.html")
